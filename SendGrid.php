4cee6c8b3afbdfdd5f2d798c97693cf4b9cad5dc
Laurentiu Craciun <laurentiu.craciun@sendgrid.com>
2013-06-07 15:17:51 +0300

    overwrite wp_email

--- SendGrid.php
+++ SendGrid.php
@@ -14,25 +14,327 @@ require_once plugin_dir_path( __FILE__ ) . '/lib/sendgrid-php/SendGrid_loader.ph
 
 $sendgridSettings = new wp_SendGrid_Settings();
 
-if (!function_exists('wp_set_current_user'))
+if (!function_exists('wp_mail'))
 {
-  function wp_mail($to, $subject, $message, $headers = '', $attachments = array() )
+  /**
+   * Send mail, similar to PHP's mail
+   *
+   * A true return value does not automatically mean that the user received the
+   * email successfully. It just only means that the method used was able to
+   * process the request without any errors.
+   *
+   * Using the two 'wp_mail_from' and 'wp_mail_from_name' hooks allow from
+   * creating a from address like 'Name <email@address.com>' when both are set. If
+   * just 'wp_mail_from' is set, then just the email address will be used with no
+   * name.
+   *
+   * The default content type is 'text/plain' which does not allow using HTML.
+   * However, you can set the content type of the email by using the
+   * 'wp_mail_content_type' filter.
+   *
+   * The default charset is based on the charset used on the blog. The charset can
+   * be set using the 'wp_mail_charset' filter.
+   *
+   * @since 1.2.1
+   * @uses apply_filters() Calls 'wp_mail' hook on an array of all of the parameters.
+   * @uses apply_filters() Calls 'wp_mail_from' hook to get the from email address.
+   * @uses apply_filters() Calls 'wp_mail_from_name' hook to get the from address name.
+   * @uses apply_filters() Calls 'wp_mail_content_type' hook to get the email content type.
+   * @uses apply_filters() Calls 'wp_mail_charset' hook to get the email charset
+   *
+   * @param string|array $to Array or comma-separated list of email addresses to send message.
+   * @param string $subject Email subject
+   * @param string $message Message contents
+   * @param string|array $headers Optional. Additional headers.
+   * @param string|array $attachments Optional. Files to attach.
+   * @return bool Whether the email contents were sent successfully.
+   */
+  function wp_mail($to, $subject, $message, $headers = '', $attachments = array())
   {
-    $user = get_option('sendgrid_user');
-    $password = get_option('sendgrid_pwd');
-    $sendgrid = new SendGrid($user, $password);
+    echo "<pre>";
+    echo "<br>to: ";      print_r($to);
+    echo "<br>subject: "; print_r($subject);
+    echo "<br>message: "; print_r($message);
+    echo "<br>headers: "; print_r($headers);
+    echo "<br>attachments: "; print_r($attachments);
+    echo "<br>================================";
 
-    $sendgrid = new SendGrid($user, $password);
-      $mail = new SendGrid\Mail();
-      $mail->addTo($to)
-           ->setFrom('me@gmail.com')
-           ->setSubject($subject)
-           ->setText($message);
-      
-      //setHtml('<strong>Hello World!</strong>');
-    return $sendgrid->web->send($mail);
+
+    $sendgrid = new SendGrid(get_option('sendgrid_user'), get_option('sendgrid_pwd'));
+    $mail = new SendGrid\Mail();
+
+    // Compact the input, apply the filters, and extract them back out
+    extract(apply_filters('wp_mail', compact('to', 'subject', 'message', 'headers', 'attachments')));
+
+    // prepare attachments
+    $attached_files = array();
+    if (!empty($attachments))
+    {
+      if (!is_array($attachments)) {
+        $pos = strpos(',', $attachments);
+        if ($pos !== false)
+        {
+          $attachments = preg_split('/,\s*/', $attachments);
+        }
+        else
+        {
+          $attachments = explode("\n", str_replace("\r\n", "\n", $attachments));
+        }
+      }
+
+      if (is_array($attachments)) {
+        foreach ($attachments as $attachment) {
+          if (file_exists($attachment)) {
+            $attached_files[] = $attachment;
+          }
+        }
+      }
+    }
+
+    // Headers
+    if (empty($headers)) {
+      $headers = array();
+    } else {
+      if (!is_array($headers)) {
+        // Explode the headers out, so this function can take both
+        // string headers and an array of headers.
+        $tempheaders = explode( "\n", str_replace( "\r\n", "\n", $headers ) );
+      } else {
+        $tempheaders = $headers;
+      }
+      $headers = array();
+      $cc = array();
+      $bcc = array();
+
+      // If it's actually got contents
+      if ( !empty( $tempheaders ) ) {
+        // Iterate through the raw headers
+        foreach ( (array) $tempheaders as $header ) {
+          if ( strpos($header, ':') === false ) {
+            if ( false !== stripos( $header, 'boundary=' ) ) {
+              $parts = preg_split('/boundary=/i', trim( $header ) );
+              $boundary = trim( str_replace( array( "'", '"' ), '', $parts[1] ) );
+            }
+            continue;
+          }
+          // Explode them out
+          list( $name, $content ) = explode( ':', trim( $header ), 2 );
+
+          // Cleanup crew
+          $name    = trim( $name    );
+          $content = trim( $content );
+
+          switch ( strtolower( $name ) ) {
+            // Mainly for legacy -- process a From: header if it's there
+            case 'from':
+              if ( strpos($content, '<' ) !== false ) {
+                // So... making my life hard again?
+                $from_name = substr( $content, 0, strpos( $content, '<' ) - 1 );
+                $from_name = str_replace( '"', '', $from_name );
+                $from_name = trim( $from_name );
+
+                $from_email = substr( $content, strpos( $content, '<' ) + 1 );
+                $from_email = str_replace( '>', '', $from_email );
+                $from_email = trim( $from_email );
+              } else {
+                $from_email = trim( $content );
+              }
+              break;
+            case 'content-type':
+              if ( strpos( $content, ';' ) !== false ) {
+                list( $type, $charset ) = explode( ';', $content );
+                $content_type = trim( $type );
+                if ( false !== stripos( $charset, 'charset=' ) ) {
+                  $charset = trim( str_replace( array( 'charset=', '"' ), '', $charset ) );
+                } elseif ( false !== stripos( $charset, 'boundary=' ) ) {
+                  $boundary = trim( str_replace( array( 'BOUNDARY=', 'boundary=', '"' ), '', $charset ) );
+                  $charset = '';
+                }
+              } else {
+                $content_type = trim( $content );
+              }
+              break;
+            case 'cc':
+              $cc = array_merge( (array) $cc, explode( ',', $content ) );
+              break;
+            case 'bcc':
+              $bcc = array_merge( (array) $bcc, explode( ',', $content ) );
+              break;
+            default:
+              // Add it to our grand headers array
+              $headers[trim( $name )] = trim( $content );
+              break;
+          }
+        }
+      }
+    }
+
+    // From email and name
+    // If we don't have a name from the input headers
+    if ( !isset( $from_name ) )
+      $from_name = get_option('sendgrid_from_name');
+
+    /* If we don't have an email from the input headers default to wordpress@$sitename
+     * Some hosts will block outgoing mail from this address if it doesn't exist but
+     * there's no easy alternative. Defaulting to admin_email might appear to be another
+     * option but some hosts may refuse to relay mail from an unknown domain. See
+     * http://trac.wordpress.org/ticket/5007.
+     */
+
+    if ( !isset( $from_email ) ) {
+      $from_email = trim(get_option('sendgrid_from_email'));
+      if (!$from_email)
+      {
+        // Get the site domain and get rid of www.
+        $sitename = strtolower( $_SERVER['SERVER_NAME'] );
+        if ( substr( $sitename, 0, 4 ) == 'www.' ) {
+          $sitename = substr( $sitename, 4 );
+        }
+
+        $from_email = 'wordpress@' . $sitename;
+      }
+    }
+
+    // Plugin authors can override the potentially troublesome default
+    $from_email = apply_filters( 'wp_mail_from'     , $from_email );
+    $from_name  = apply_filters( 'wp_mail_from_name', $from_name  );
+
+    // Set destination addresses
+    if ( !is_array( $to ) )
+      $to = explode( ',', $to );
+
+
+//    // Add any CC and BCC recipients
+//    if ( !empty( $cc ) ) {
+//      foreach ( (array) $cc as $recipient ) {
+//        try {
+//          // Break $recipient into name and address parts if in the format "Foo <bar@baz.com>"
+//          $recipient_name = '';
+//          if( preg_match( '/(.*)<(.+)>/', $recipient, $matches ) ) {
+//            if ( count( $matches ) == 3 ) {
+//              $recipient_name = $matches[1];
+//              $recipient = $matches[2];
+//            }
+//          }
+//
+//          //$phpmailer->AddCc( $recipient, $recipient_name );
+//        } catch ( phpmailerException $e ) {
+//          continue;
+//        }
+//      }
+//    }
+
+//    if ( !empty( $bcc ) ) {
+//      foreach ( (array) $bcc as $recipient) {
+//        try {
+//          // Break $recipient into name and address parts if in the format "Foo <bar@baz.com>"
+//          $recipient_name = '';
+//          if( preg_match( '/(.*)<(.+)>/', $recipient, $matches ) ) {
+//            if ( count( $matches ) == 3 ) {
+//              $recipient_name = $matches[1];
+//              $recipient = $matches[2];
+//            }
+//          }
+//          $phpmailer->AddBcc( $recipient, $recipient_name );
+//        } catch ( phpmailerException $e ) {
+//          continue;
+//        }
+//      }
+//    }
+
+    // Set Content-Type and charset
+    // If we don't have a content-type from the input headers
+    if ( !isset( $content_type ) )
+      $content_type = 'text/plain';
+
+    $content_type = apply_filters( 'wp_mail_content_type', $content_type );
+
+//    // Set whether it's plaintext, depending on $content_type
+//    if ( 'text/html' == $content_type )
+//      $phpmailer->IsHTML( true );
+
+//    // If we don't have a charset from the input headers
+//    if ( !isset( $charset ) )
+//      $charset = get_bloginfo( 'charset' );
+//
+//    // Set the content-type and charset
+//    $phpmailer->CharSet = apply_filters( 'wp_mail_charset', $charset );
+
+//    // Set custom headers
+//    if ( !empty( $headers ) ) {
+//      foreach( (array) $headers as $name => $content ) {
+//        $phpmailer->AddCustomHeader( sprintf( '%1$s: %2$s', $name, $content ) );
+//      }
+//
+//      if ( false !== stripos( $content_type, 'multipart' ) && ! empty($boundary) )
+//        $phpmailer->AddCustomHeader( sprintf( "Content-Type: %s;\n\t boundary=\"%s\"", $content_type, $boundary ) );
+//    }
+
+//    do_action_ref_array( 'phpmailer_init', array( &$phpmailer ) );
+
+
+
+//    echo "<br>to: ";      print_r($to);
+//    echo "<br>subject: "; print_r($subject);
+//    echo "<br>message: "; print_r($message);
+//    echo "<br>message: "; print_r($from_email);
+//
+//    echo "<br>headers: "; print_r($headers);
+//    echo "<br>attachments: "; print_r($attachments);
+//    echo "<br>================================";
+
+
+    $mail->setTos($to)
+         ->setSubject($subject)
+         ->setText($message)
+         ->setFrom($from_email);
+
+    // send HTML content
+	  if ($content_type !== 'text/plain')
+    {
+		  $mail->setHtml($message);
+    }
+    // set from name
+    if ($from_email)
+    {
+      $mail->setFromName($from_name);
+    }
+    // set from cc
+    if (count($cc))
+    {
+      $mail->setCcs($cc);
+    }
+    // set from bcc
+    if (count($bcc))
+    {
+      $mail->setBccs($bcc);
+    }
+    // add attachemnts
+    if (count($attached_files))
+    {
+      $mail->setAttachments($attached_files);
+    }
+var_dump($mail);
+
+
+    // Send!
+    try
+    {
+      return $sendgrid->web->send($mail);
+    }
+    catch (Exception $e)
+    {
+      return false;
+    }
+
+    return true;
   }
-}
 
-///$mail = wp_mail('laurentiu.craciun@sendgrid.com', 'test plugin', 'testing wordpress plugin');
+}
+else
+{
+  // Mandrill: wp_mail has been declared by another process or plugin, so you won't be able to use Mandrill until the problem is solved.
+  return false;
+}
+//$mail = wp_mail('laurentiu.craciun@sendgrid.com', 'test plugin', 'testing wordpress plugin');
 //var_dump($mail);
