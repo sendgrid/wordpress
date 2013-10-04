282282277c16d9a9bfd03efe2e9fba87baa84b9e
Daniela Mihalache <daniela.mihalache@sendgrid.com>
2013-10-04 13:37:59 +0300

    add plugin version from svn

---
+++ trunk/view/partials/sendgrid_stats_deliveries.php
@@ -0,0 +1,12 @@
+<div id="sendgrid_statistics_deliveries_widget" class="postbox ">
+  <h3 class="hndle"><span>SendGrid Deliveries</span></h3>
+  <div class="inside">
+
+    <div class="sendgrid-container" style="position:relative;">
+      <img src="<?= plugin_dir_url(__FILE__); ?>../images/loader.gif" class="loading" style="position:absolute;" />
+      <div id="deliveries-container" class="sendgrid-stats"></div>
+    </div>
+    <div id="deliveries-container-legend" class="sendgrid-stats-legend"></div> 
+
+  </div>
+</div>
